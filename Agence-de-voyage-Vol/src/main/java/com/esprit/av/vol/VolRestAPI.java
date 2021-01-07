28cb455b3eec25fe5771e44efe5b128c8170d852
atef <atef.hafdhi@esprit.tn>
2021-01-07 19:18:43 +0100

    Reservation micro-services
    Adding reservation CRUD , a client will be able to select to make a reservation by choosing the accomodation , flight and a specific day .

--- Agence-de-voyage-Vol/src/main/java/com/esprit/av/vol/VolRestAPI.java
+++ Agence-de-voyage-Vol/src/main/java/com/esprit/av/vol/VolRestAPI.java
@@ -1,17 +1,23 @@
 package com.esprit.av.vol;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import Main.Reservation;
 
 @RestController
 @RequestMapping(value = "/api/vols")
@@ -30,10 +36,17 @@ public class VolRestAPI {
 		return title;
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Vol> createVol(@RequestBody Vol vol) {
 		return new ResponseEntity<>(volService.addVol(vol), HttpStatus.OK);
+	}
+	
+	@GetMapping("/retrieve-reservations")
+	@ResponseBody
+	public List<Vol> getReservations() {
+		List<Vol>  list =  volService.retrieveAllReservations(); 
+		return list;
 	}
 	
 	
--- Agence-de-voyage-Vol/src/main/java/com/esprit/av/vol/VolService.java
+++ Agence-de-voyage-Vol/src/main/java/com/esprit/av/vol/VolService.java
@@ -1,11 +1,18 @@
 package com.esprit.av.vol;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+
+
+
 @Service
 public class VolService {
-	
+	private static final org.apache.logging.log4j.Logger l= LogManager.getLogger(VolService.class);
 	
 	@Autowired
 	private VolRepository volRepository;
@@ -30,6 +37,13 @@ public class VolService {
 			return "vol supprimé";
 		} else
 			return "vol non supprimé";
+	}
+	public List<Vol> retrieveAllReservations() {
+		List<Vol> vols= (List<Vol>) volRepository.findAll();
+		for (Vol vol : vols){
+			l.debug("Reservation ++++ : "+vol);
+		}
+		return vols;
 	}
 
 }
